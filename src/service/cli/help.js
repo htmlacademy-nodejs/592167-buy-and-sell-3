684408f606affa66c9108f8ca4b2c5b880fb98b4
Agafonov Alexey <agafonov@fgosniias.ru>
2020-02-06 11:38:18 +0300

    Добавляет в модуль help пакет chalk и расскрашивает выводы в серый цвет.

--- src/service/cli/help.js
+++ src/service/cli/help.js
@@ -1,13 +1,15 @@
 'use strict';
 
+const chalk = require(`chalk`);
+
 const helpText = () => (
-  console.log(`Программа формирует файл с данными для API.
+  console.log(chalk.gray(`Программа формирует файл с данными для API.
 
     Команды:
     --version:            выводит номер версии
     --help:               печатает этот текст
     --generate <count>    формирует файл mock.json
-  `)
+  `))
 );
 
 module.exports = {
