a1bf9a1e8de995b4e68b956f862051e1c775ce22
Agafonov Alexey <agafonov@fgosniias.ru>
2020-02-26 16:13:35 +0300

    Добавляет директорию templates для шаблонов. Фиксирует директорию с шаблонами и указывает что в качестве шаблонизатора используем pug.

--- src/express/index.js
+++ src/express/index.js
@@ -4,6 +4,8 @@ const express = require(`express`);
 const {initializeRoutes} = require(`./routes/index`);
 
 const app = express();
+app.set(`views`, `./templates`);
+app.set(`view engine`, `pug`);
 initializeRoutes(app);
 
 const port = 8080;
