fa797744eab1b08593a7c35d9eecfe2d4ff71933
Agafonov Alexey <agafonov@fgosniias.ru>
2020-03-10 14:17:43 +0300

    При помощи Router создает get маршрут /offers, при обращении к которому возвращается JSON файл или пустой массив.

--- src/service/routes/offers.js
+++ src/service/routes/offers.js
@@ -1,14 +1,26 @@
 'use strict';
 
-const fs = require(`fs`).promises;
+const fs = require(`fs`);
 const {Router} = require(`express`);
 const router = new Router();
 
 const {MOCK_FILE_NAME} = require(`../../constants`);
 
 router.get(`/`, async (req, res) => {
-  const fileContent = await fs.readFile(MOCK_FILE_NAME);
-  res.json(JSON.parse(fileContent).map((el) => el.title));
+  try {
+    fs.accessSync(MOCK_FILE_NAME, fs.constants.F_OK, (err) => {
+      if (err) throw err;
+    });
+    const fileStat = fs.statSync(MOCK_FILE_NAME);
+    if (fileStat.size > 0) {
+      const fileContent = fs.readFileSync(MOCK_FILE_NAME);
+      res.json(JSON.parse(fileContent).map((el) => el.title));
+    } else {
+      res.send([]);
+    }
+  } catch (err) {
+    res.send([]);
+  }
 });
 
 module.exports = router;
