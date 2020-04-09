ee1714e84e27d6cf62a0d9acacacbc66e16bef60
Agafon <dragafony@gmail.com>
2020-04-09 10:02:25 +0300

    Меняет content на announcements.

--- src/service/repositories/announcement.js
+++ src/service/repositories/announcement.js
@@ -4,34 +4,34 @@ const fs = require(`fs`);
 const {deleteItemFromArray, getNewId} = require(`../../utils`);
 
 const {MOCK_FILE_NAME} = require(`../../constants`);
-let content = fs.existsSync(MOCK_FILE_NAME) ? JSON.parse(fs.readFileSync(MOCK_FILE_NAME)) : [];
+let announcements = fs.existsSync(MOCK_FILE_NAME) ? JSON.parse(fs.readFileSync(MOCK_FILE_NAME)) : [];
 
-const findById = (id) => content.find((el) => el.id === id);
+const findById = (id) => announcements.find((el) => el.id === id);
 
 const exists = (id) => findById(id) !== undefined;
 
-const findAll = () => content;
+const findAll = () => announcements;
 
 const save = (newAnnouncement, id) => {
   if (id) {
-    const announcement = content.find((el) => el.id === id);
-    const newContent = deleteItemFromArray(content, id);
+    const announcement = announcements.find((el) => el.id === id);
+    const newContent = deleteItemFromArray(announcements, id);
     const newOffer = Object.assign({}, announcement, newAnnouncement);
     newContent.push(newOffer);
-    content = newContent;
+    announcements = newContent;
   } else {
     newAnnouncement.id = getNewId();
-    content.push(newAnnouncement);
+    announcements.push(newAnnouncement);
   }
   return newAnnouncement.id;
 };
 
 const remove = (id) => {
-  content = deleteItemFromArray(content, id);
+  announcements = deleteItemFromArray(announcements, id);
 };
 
 const findByTitle = (queryString) => {
-  return content.filter((el) => el.title.toUpperCase().includes(queryString.toUpperCase()));
+  return announcements.filter((el) => el.title.toUpperCase().includes(queryString.toUpperCase()));
 };
 
 
