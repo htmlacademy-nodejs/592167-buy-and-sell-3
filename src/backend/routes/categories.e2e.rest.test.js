c0335faa9d594f20edf7c0b7dc004606da6e630d
Agafon <dragafony@gmail.com>
2020-04-20 18:38:33 +0300

    Добавляет e2e тесты для categories.

---
+++ src/backend/routes/categories.e2e.rest.test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const request = require(`supertest`);
+const app = require(`../app`);
+
+describe(`categories`, () => {
+  test(`get all categories`, async () => {
+    const res = await request(app).get(`/api/categories`);
+
+    expect(res.statusCode).toBe(200);
+  });
+});
---
+++ src/backend/routes/search.e2e.rest.test.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const request = require(`supertest`);
+const app = require(`../app`);
+
+const newAnnouncement = {
+  title: `some title`,
+  comments: [
+    {
+      id: `yCjvjj`,
+      text: ``
+    },
+    {
+      id: `KZ5IcZ`,
+      text: `Оплата наличными или перевод на карту? Продаю в связи с переездом. Отрываю от сердца.`
+    },
+    {
+      id: `XswJI4`,
+      text: `А где блок питания. С чем связана продажа? Почему так дешёво?`
+    }
+  ],
+  description: `Пользовались бережно и только по большим праздникам.`,
+  picture: `item11.jpg`,
+  type: `SALE`,
+  sum: 54355
+};
+
+describe(`search title`, () => {
+  test(`search announcements by substring`, async () => {
+    const tempAnnouncement = await request(app).post(`/api/offers`)
+      .send(newAnnouncement);
+    const res = await request(app).get(`/api/search?query=title`);
+
+    expect(res.body[0].id).toBe(tempAnnouncement.body.id);
+  });
+});
