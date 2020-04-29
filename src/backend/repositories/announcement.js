68995f56f7e8353a565e2c8b69eeda6c318591f2
Agafon <dragafony@gmail.com>
2020-04-29 16:57:43 +0300

    Удаляет метод set из repositories/announcement, за не востребованностью.

--- src/backend/repositories/announcement.js
+++ src/backend/repositories/announcement.js
@@ -34,10 +34,6 @@ const findByTitle = (queryString) => {
   return announcements.filter((el) => el.title.toUpperCase().includes(queryString.toUpperCase()));
 };
 
-const set = (newAnnouncements) => {
-  announcements = newAnnouncements;
-};
-
 
 module.exports = {
   exists,
@@ -46,5 +42,4 @@ module.exports = {
   save,
   findByTitle,
   remove,
-  set,
 };
