7a80b70a5a8c0fd5fb1c72601f0f4d228afc2698
Agafon <dragafony@gmail.com>
2020-05-02 11:24:50 +0300

    Убирает подготовку данных через апи. Добавляет методы: addMockAnnouncement и deleteMockAnnouncement.

--- src/backend/routes/search.e2e.rest.test.js
+++ src/backend/routes/search.e2e.rest.test.js
@@ -2,35 +2,55 @@
 
 const request = require(`supertest`);
 const app = require(`../app`);
+const announcementRepository = require(`../repositories/announcement`);
 
-const newAnnouncement = {
-  title: `some title`,
+const MOCK_ANNOUNCEMENT = {
+  categories: [
+    `Посуда`,
+    `Программирование`
+  ],
+  description: `Это настоящая находка для коллекционера! Задача организации, в особенности же постоянное информационно-техническое обеспечение нашей деятельности требует от нас анализа модели. Повседневная практика показывает, что консультация с профессионалами из IT играет важную роль в формировании позиций. Даю недельную гарантию.`,
+  picture: `item08.jpg`,
+  title: `Самый остроумный заголовок, который я смог придумать.`,
+  type: `OFFER`,
+  sum: 64652,
   comments: [
     {
-      id: `yCjvjj`,
-      text: ``
+      id: `EvDdCm`,
+      text: `Почему в таком ужасном состоянии? Неплохо, но дорого.`
     },
     {
-      id: `KZ5IcZ`,
-      text: `Оплата наличными или перевод на карту? Продаю в связи с переездом. Отрываю от сердца.`
+      id: `z65rvX`,
+      text: `Продаю в связи с переездом. Отрываю от сердца. Вы что?! В магазине дешевле. Совсем немного...  А сколько игр в комплекте? Неплохо, но дорого. А где блок питания. Почему в таком ужасном состоянии?`
     },
     {
-      id: `XswJI4`,
-      text: `А где блок питания. С чем связана продажа? Почему так дешёво?`
-    }
-  ],
-  description: `Пользовались бережно и только по большим праздникам.`,
-  picture: `item11.jpg`,
-  type: `SALE`,
-  sum: 54355
+      id: `nFBrDX`,
+      text: `В магазине дешевле.`
+    },
+    {
+      id: `-OB7D4`,
+      text: `Продаю в связи с переездом. Отрываю от сердца. Совсем немного... Неплохо, но дорого. А сколько игр в комплекте? Почему в таком ужасном состоянии?`
+    },
+    {
+      id: `C6DDtC`,
+      text: `Вы что?! В магазине дешевле. А сколько игр в комплекте?`
+    },
+  ]
 };
+
+const addMockAnnouncement = () => announcementRepository.save(MOCK_ANNOUNCEMENT);
+const deleteMockAnnouncement = (id) => announcementRepository.remove(id);
 
 describe(`search title`, () => {
   test(`search announcements by substring`, async () => {
-    const tempAnnouncement = await request(app).post(`/api/offers`)
-      .send(newAnnouncement);
-    const res = await request(app).get(encodeURI(`/api/search?query=title`));
+    const id = addMockAnnouncement();
+    const expectedAnnouncement = Object.assign({}, MOCK_ANNOUNCEMENT);
+    expectedAnnouncement.id = id;
 
-    expect(res.body[0].id).toBe(tempAnnouncement.body.id);
+    const res = await request(app).get(encodeURI(`/api/search?query=Самый остроумный`));
+
+    expect(res.body[0]).toEqual(expectedAnnouncement);
+
+    deleteMockAnnouncement(id);
   });
 });
