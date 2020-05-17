b59e5d06c2b58feb489ccd0d0efe885f4a9d7cbd
Agafon <dragafony@gmail.com>
2020-05-17 19:37:46 +0300

    Делает запрос комментариев первых трех объявлений.

--- src/frontend/routes/my.js
+++ src/frontend/routes/my.js
@@ -6,14 +6,21 @@ const router = new Router();
 const request = require(`request-promise-native`);
 const {MOCK_URL} = require(`../../constants`);
 
+const MAX_ANNOUNCEMENTS_COMMENTS = 3;
+
 router.get(`/`, (req, res) => {
   request(`${MOCK_URL}/api/offers`, {json: true})
     .then((content) => console.log(content));
   res.send(req.originalUrl);
 });
 router.get(`/comments`, (req, res) => {
-  // request(`${MOCK_URL}/api/offers/:offerId/comments`, {json: true})
-  //   .then((content) => console.log(content));
+  request(`${MOCK_URL}/api/offers`, {json: true})
+    .then((announcements) => {
+      const commentsOnAnnouncements = announcements.length > MAX_ANNOUNCEMENTS_COMMENTS ? announcements.slice(0, MAX_ANNOUNCEMENTS_COMMENTS) : announcements;
+      const comments = commentsOnAnnouncements.map((it) => it.comments);
+      console.log(comments);
+    });
+  console.log(`We are here`);
   res.send(req.originalUrl);
 });
 
