8260af4ab85a8669ca8e135070452fb8313f6f07
Agafon <dragafony@gmail.com>
2020-05-20 17:21:29 +0300

    Меняет статику на динамические значения для страницы comments.

--- src/frontend/routes/my.js
+++ src/frontend/routes/my.js
@@ -17,10 +17,15 @@ router.get(`/comments`, (req, res) => {
   request(`${MOCK_URL}/api/offers`, {json: true})
     .then((announcements) => {
       const commentsOnAnnouncements = announcements.length > MAX_ANNOUNCEMENTS_COMMENTS ? announcements.slice(0, MAX_ANNOUNCEMENTS_COMMENTS) : announcements;
-      const comments = commentsOnAnnouncements.map((it) => it.comments);
-    });
-  res.render(`comments`);
-  // res.send(req.originalUrl);
+      const commentsList = commentsOnAnnouncements.map((it) => ({
+        title: it.title,
+        type: it.type,
+        sum: it.sum,
+        comments: it.comments
+      }));
+      res.render(`comments`, {commentsList});
+    })
+    .catch((err) => res.render(`500`, {err}));
 });
 
 module.exports = router;
