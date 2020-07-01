b8ff96d744bbe0c7786f0a4391266c6fe6c7a48e
Agafon <dragafony@gmail.com>
2020-07-01 23:34:59 +0300

    Добавляет файл fill-db.sql, для заполнения базы данными.

---
+++ fill-db.sql
@@ -0,0 +1,8 @@
+-- Добавляет типы объявлений
+insert into types values (default, 'Купить'),
+    (default, 'Продать');
+
+-- Добавляет пользователей
+insert into users values (default, 'Иванов', 'Иван', 'my@mail.ru', '123455'),
+                         (default, 'Петров', 'Петр', 'petrov@gmail.com', '34523'),
+                         (default, 'Сидоров', 'Илья', 'sidorov.iliya@yahoo.com', 'I8e#8d');
