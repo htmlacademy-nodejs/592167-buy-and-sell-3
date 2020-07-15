dbacc40ff2a44df7db7a1ed90e93358ad23df2eb
Agafon <dragafony@gmail.com>
2020-07-15 15:50:54 +0300

    Добавляет недостающие запросы в файл queries.sql.

--- queries.sql
+++ queries.sql
@@ -39,3 +39,53 @@ left join users u
 group by a.id, a.title, a.sum, t.type, a.description, a.regdate,
          u.last_name, u.first_name, u.email
 order by a.regdate desc;
+
+-- Получить полную информацию определённого объявления
+-- (идентификатор объявления, заголовок объявления, стоимость, тип объявления,
+-- текст объявления, дата публикации, имя и фамилия автора, контактный email,
+-- количество комментариев, наименование категорий)
+select a.id, a.title, a.sum, t.type, a.description, a.regdate,
+       concat(u.last_name, ' ', u.first_name) as "user", u.email,
+       count(cm.announcement_id) as comments,
+       string_agg(c.category, ', ') as categories
+from announcements_to_categories ac
+         left join announcements a
+                   on ac.announcement_id = a.id and a.id = 3
+         left join comments cm
+                   on a.id = cm.announcement_id
+         left join types t
+                   on a.type_id = t.id
+         left join categories c
+                   on ac.category_id = c.id
+         left join users u
+                   on a.user_id = u.id
+group by a.id, a.title, a.sum, t.type, a.description, a.regdate,
+         u.last_name, u.first_name, u.email;
+
+-- Получить список из 5 свежих комментариев
+-- (идентификатор комментария, идентификатор объявления, имя и фамилия автора, текст комментария)
+select c.id, c.announcement_id, concat(u.last_name, ' ', u.first_name) as "user", c.comment
+from comments c, users u
+where c.user_id=u.id
+group by c.id, c.regdate, c.announcement_id, u.last_name, u.first_name
+order by c.regdate desc
+limit 5
+
+
+-- Получить список комментариев для определённого объявления
+-- (идентификатор комментария, идентификатор объявления, имя и фамилия автора, текст комментария).
+-- Сначала новые комментарии;
+select c.id, c.announcement_id, concat(u.last_name, ' ', u.first_name) as "user", c.comment
+from comments c, users u
+where c.user_id=u.id and c.announcement_id=2
+group by c.id, c.regdate, c.announcement_id, u.last_name, u.first_name
+order by c.regdate desc
+
+-- Выбрать 2 объявления, соответствующих типу «куплю»;
+select *
+from announcements
+where type_id=1
+limit 2
+
+-- Обновить заголовок определённого объявления на «Уникальное предложение!»;
+update announcements set title='Уникальное предложение!' where id=1
