bc40140317d1be8be56cd75a873332e8b478870e
Agafonov Alexey <agafonov@fgosniias.ru>
2020-07-23 12:49:19 +0300

    Подключает модуль соединения с СУБД и настраивает его так, чтобы он стартовал при запуске сервера.

--- src/cli/server.js
+++ src/cli/server.js
@@ -1,11 +1,13 @@
 'use strict';
 
 const app = require(`../backend/app`);
+const {testConnect} = require(`../backend/services/db-connect`);
 const {getLogger} = require(`../backend/logger`);
 require(`dotenv`).config();
 
 const logger = getLogger();
 const DEFAULT_PORT = 3000;
+
 
 module.exports = {
   name: `--server`,
@@ -13,6 +15,7 @@ module.exports = {
     const port = process.env.SERVER_PORT || DEFAULT_PORT;
 
     app.listen(port, () => {
+      testConnect();
       logger.info(`Сервер запущен на порту: ${port}`);
     }).on(`error`, (err) => {
       logger.error(`Сервер не смог запуститься. Ошибка ${err}`);
