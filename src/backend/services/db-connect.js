30abad4726e922d1b09db4cc6368e4f9486c0bba
Agafonov Alexey <agafonov@fgosniias.ru>
2020-07-23 13:32:51 +0300

    Добавляет логирование при подключении к СУБД.

--- src/backend/services/db-connect.js
+++ src/backend/services/db-connect.js
@@ -1,15 +1,19 @@
 'use strict';
 
 const Sequelize = require(`sequelize`);
-
 require(`dotenv`).config();
+
+const {getLogger} = require(`../logger`);
+const logger = getLogger();
 
 const sequelize = new Sequelize(`${process.env.DB_NAME}`, `${process.env.DB_USER}`,
   `${process.env.USER_PASSWORD}`,
   {
-  host: `${process.env.DB_HOST}`,
-  dialect: `${process.env.DIALECT}`,
-});
+    host: `${process.env.DB_HOST}`,
+    dialect: `${process.env.DIALECT}`,
+    logging: (msg) => logger.info(msg),
+  }
+);
 
 const testConnect = async () => {
   try {
