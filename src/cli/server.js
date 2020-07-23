6e3e52d5beb29d4af9016e68f292eb3a3d0b9712
Agafonov Alexey <agafonov@fgosniias.ru>
2020-07-23 10:40:14 +0300

    Переделывает запуск бэка на использование dotenv.

--- src/cli/server.js
+++ src/cli/server.js
@@ -2,14 +2,15 @@
 
 const app = require(`../backend/app`);
 const {getLogger} = require(`../backend/logger`);
+require(`dotenv`).config();
+
 const logger = getLogger();
 const DEFAULT_PORT = 3000;
 
 module.exports = {
   name: `--server`,
-  run(args) {
-    const [customPort] = args;
-    const port = Number.parseInt(customPort, 10) || DEFAULT_PORT;
+  run() {
+    const port = process.env.SERVER_PORT || DEFAULT_PORT;
 
     app.listen(port, () => {
       logger.info(`Сервер запущен на порту: ${port}`);
