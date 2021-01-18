20914b981203dc7a26dfb1836eec6b8c6b1d76a4
Agafon <dragafony@gmail.com>
2021-01-18 08:58:11 +0300

    Добавляет middleware для приватных путей.

---
+++ src/middleware/private.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const axios = require(`axios`);
+const {BACKEND_URL} = require(`../constants`);
+
+module.exports = () => (
+  async (req, res, next) => {
+    if (req.session) {
+      try {
+        if (req.session.isLogged) {
+          await axios.post(`${BACKEND_URL}/api/session/check-login`, {
+            username: req.session.username,
+          });
+        }
+        next();
+      } catch (err) {
+        req.session.destroy();
+        res.redirect(`/login`);
+      }
+    }
+  }
+);
