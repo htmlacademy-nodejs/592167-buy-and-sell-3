239fe5c3ad9b2ff9418ebcef0eb4e3b4bd6d5be4
Agafon <dragafony@gmail.com>
2021-01-27 09:52:51 +0300

    Покрывает тестами модуль categories.js

--- src/backend/services/categories.test.js
+++ src/backend/services/categories.test.js
@@ -5,13 +5,25 @@ jest.mock(`../repositories/categories`);
 
 const underTest = require(`./categories`);
 
+const {CATEGORY_ID, DEFAULT} = require(`../../constants`);
+
 describe(`getCategories`, () => {
-  test(`test should return categories`, () => {
+  test(`test should return categories`, async () => {
     const expectedCategories = [``, ``, ``];
     categoriesRepository.findAll.mockReturnValue(expectedCategories);
 
-    const actual = underTest.getCategories();
+    const actual = await underTest.getCategories();
 
     expect(actual).toEqual(expectedCategories);
+  });
+});
+
+describe(`getAnnouncementsOfCategory`, () => {
+  test(`test should return announcements of category`, async () => {
+    const expectedAnnouncements = [{}, {}, {}];
+    categoriesRepository.getAnnouncementsOfCategories.mockReturnValue(expectedAnnouncements);
+
+    const announcements = await underTest.getAnnouncementsOfCategory(CATEGORY_ID, DEFAULT.PREVIEW_COUNT);
+    expect(announcements).toEqual(expectedAnnouncements);
   });
 });
